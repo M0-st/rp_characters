caaa405808a0de5a0bfe8da90719b1a9f5ceef0c
Matthias Oudart <matthias.oudart@gmail.com>
2025-02-21 11:38:25 +0100

    normalment db clean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_21_101408) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_21_103633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,14 +25,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_21_101408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_characters_on_user_id"
-  end
-
-  create_table "members", force: :cascade do |t|
-    t.string "pseudo"
-    t.string "photo"
-    t.boolean "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
