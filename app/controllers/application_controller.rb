63dfadcb903f504be1964dd23a783208a7f4d34c
Matthias Oudart <matthias.oudart@gmail.com>
2025-02-22 01:35:18 +0100

    normalement tout marche sauf  le login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[pseudonym role photo password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:pseudonym])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[pseudonym role photo password current_password])
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   def to_s
     pseudonym
   end
+  
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    pseudonym = conditions.delete(:pseudonym)  # Correction : assignation à pseudonym
+    if pseudonym
+      where(conditions).where(["pseudonym = ?", pseudonym]).first
+    else
+      where(conditions).first
+    end
+  end
 end
